fe5d7441d2865b39b2f657b05730fe092752261b
LePichu <ishat.prsnl65844@gmail.com>
2021-02-04 17:30:14 +0530

    attempts at blockentity

---
+++ src/main/java/com/remodstudios/lumidep/Blocks/BeastBlock.java
@@ -0,0 +1,8 @@
+package com.remodstudios.lumidep.Blocks.BlockEntity;
+import net.minecraft.block.Block;
+
+public class GorgeMassBlock extends Block {
+    public GorgeMassBlock(Settings settings) {
+		super(settings);
+    }
+}
