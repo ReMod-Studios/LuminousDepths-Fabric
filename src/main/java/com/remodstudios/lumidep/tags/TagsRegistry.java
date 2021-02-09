df48f608db9588c6d4e2a229e0f9b8d417172282
Platymemo <edwardhazelton+github@gmail.com>
2021-02-08 23:10:40 -0900

    Fixed tag

--- src/main/java/com/remodstudios/lumidep/tags/TagsRegistry.java
+++ src/main/java/com/remodstudios/lumidep/tags/TagsRegistry.java
@@ -6,12 +6,12 @@ import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 
-// Have to name tagsRegistry cause FAPI's registry section is called TagRegistry
+// Have to name TagsRegistry cause FAPI's registry section is called TagRegistry
 public class TagsRegistry {
     public static final Tag<Item> SHINY = TagRegistry.item(LuminousDepths.id("shiny"));
 
     public static void generateData(ArtificeResourcePack.ServerResourcePackBuilder pack) {
-        pack.addBlockTag(LuminousDepths.id("shiny"), tag -> tag
+        pack.addItemTag(LuminousDepths.id("shiny"), tag -> tag
             // yes im using raw json :yeehaw: - leocth
             .values(
                 LuminousDepths.id("tungsten_block"),
