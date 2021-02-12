5340537a6a145218f9d98b96c495d94845b221cf
Platymemo <edwardhazelton+github@gmail.com>
2021-02-12 14:57:46 -0900

    small gorgebeast changes

--- src/main/java/com/remodstudios/lumidep/client/render/entity/models/GorgeBeastEntityModel.java
+++ src/main/java/com/remodstudios/lumidep/client/render/entity/models/GorgeBeastEntityModel.java
@@ -4,9 +4,8 @@ package com.remodstudios.lumidep.client.render.entity.models;
 import com.remodstudios.lumidep.LuminousDepths;
 import com.remodstudios.lumidep.entities.GorgeBeastEntity;
 import net.minecraft.util.Identifier;
-import software.bernie.geckolib3.model.AnimatedGeoModel;
 
-public class GorgeBeastEntityModel extends AnimatedGeoModel<GorgeBeastEntity> {
+public class GorgeBeastEntityModel extends SwimmingGeoModel<GorgeBeastEntity> {
     private static Identifier MODEL = LuminousDepths.id("geo/gorgebeast.geo.json");
     private static Identifier TEXTURE = LuminousDepths.id("textures/entity/gorgebeast.png");
     private static Identifier ANIMATION = LuminousDepths.id("animations/gorgebeast.animation.json");
@@ -23,7 +22,6 @@ public class GorgeBeastEntityModel extends AnimatedGeoModel<GorgeBeastEntity> {
 
     @Override
     public Identifier getAnimationFileLocation(GorgeBeastEntity gorgeBeastEntity) {
-        // No animations yet
-        return null;
+        return ANIMATION;
     }
 }
